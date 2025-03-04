569602fba5c47d240305d3db5728b2d48466939e
Wally0517 <165603962+Wally0517@users.noreply.github.com>
2025-03-03 19:31:51 -0600

    Create Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/projects">Projects</router-link></li>
+      <li><router-link to="/contact">Contact</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+nav {
+  background: #0044cc;
+  padding: 1rem;
+}
+nav ul {
+  display: flex;
+  list-style: none;
+  gap: 1rem;
+}
+nav ul li a {
+  color: white;
+  text-decoration: none;
+}
+</style>
