8cf5f020053add73046ba389bb0ac26bb83c65c5
Wally0517 <165603962+Wally0517@users.noreply.github.com>
2025-03-03 19:32:29 -0600

    Create Hero.vue

---
+++ src/components/Hero.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1>Welcome to BrightWave Enterprises</h1>
+    <p>Premium Student Housing in Nigeria</p>
+  </section>
+</template>
+
+<style>
+section {
+  text-align: center;
+  padding: 2rem;
+  background: lightblue;
+}
+</style>
