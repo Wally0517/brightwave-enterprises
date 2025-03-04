3f33be5645365132612bc4aecc61d5b895bc4058
Wally0517 <165603962+Wally0517@users.noreply.github.com>
2025-03-03 21:10:43 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router"; // Import Vue Router
+import "./style.css"; // Import global styles
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router); // Use Vue Router
+app.mount("#app");
