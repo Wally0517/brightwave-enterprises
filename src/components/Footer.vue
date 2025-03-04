45aac275dd3c4f3dd11b49f6b1972021d58801ef
Wally0517 <165603962+Wally0517@users.noreply.github.com>
2025-03-03 19:33:34 -0600

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <p>&copy; 2025 BrightWave Enterprises. All rights reserved.</p>
+  </footer>
+</template>
+
+<style>
+footer {
+  background: #0044cc;
+  color: white;
+  text-align: center;
+  padding: 1rem;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+</style>
