899b4a95befe0333f2bb6bda43c07846f0587195
Wally0517 <165603962+Wally0517@users.noreply.github.com>
2025-03-03 22:55:49 -0600

    Update AboutUs.vue

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -4,6 +4,7 @@
     <section class="hero-section">
       <div class="hero-content">
         <img src="@/assets/brightwave-logo.png" alt="BrightWave Logo" class="logo" />
+        <img src="/assets/hero-about.jpg" alt="Hero Image">
         <h1>About BrightWave Enterprises</h1>
         <p>Redefining student housing with comfort, security, and innovation.</p>
       </div>
