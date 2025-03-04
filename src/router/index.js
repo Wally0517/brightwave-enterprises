864dc9b23b9a9b1496249584009b172391df500a
Wally0517 <165603962+Wally0517@users.noreply.github.com>
2025-03-03 21:08:50 -0600

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../pages/Home.vue";
-import About from "../pages/About.vue";
+import Home from "@/pages/Home.vue";
+import About from "@/pages/About.vue";
 
 const routes = [
-  { path: "/", component: Home },
-  { path: "/about", component: About },
+  { path: "/", name: "Home", component: Home },
+  { path: "/about", name: "About", component: About },
 ];
 
 const router = createRouter({
