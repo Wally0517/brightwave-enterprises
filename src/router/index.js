3c2dbe318e776efbd58649280b6cfb7656c8869f
Wally0517 <165603962+Wally0517@users.noreply.github.com>
2025-03-09 14:55:34 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/pages/Home.vue";
 import About from "@/pages/About.vue";
+import Admin from "@/pages/Admin.vue"; // Import Admin Panel
 
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/about", name: "About", component: About },
+  { path: "/admin", name: "Admin", component: Admin }, // Admin route
 ];
 
 const router = createRouter({
